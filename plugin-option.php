b9753b5c4224f1d49ad2048f66a11018f3c8a6b4
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-06-04 17:19:43 +0600

    Plugin Created

---
+++ plugin-option.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Plugin Option
+ * Author: SaberHR
+ * Plugin URI: http://saberhr.com
+ * Author URI: http://saberhr.com
+ * Description:
+ * Version: 1.0.0
+ * Licence: GPLv2 or later
+ * Text Domain: plugin-option
+ * Domain Path: /language/
+ */
+
